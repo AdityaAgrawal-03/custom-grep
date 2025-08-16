596f2f1a5a8b66378dafe8901613e37000b1bf6b
Aditya Agrawal <aditya.agrawal@airmeet.com>
2025-08-16 13:40:48 +0530

    feat: initial commit

---
+++ src/go_grep.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	recursivePtr := flag.Bool("r", false, "recursive search flag")
+	invertPtr := flag.Bool("v", false, "inverted search flag")
+
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) < 1 {
+		log.Fatalln("Usage: gogrep [-r] [-v] <pattern> [filename|directory]")
+	}
+
+	pattern := args[0]
+	// Compile regex once
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		log.Fatalf("Invalid regular expression: %s", err)
+	}
+
+	fi, _ := os.Stdin.Stat()
+
+	// check if input is piped
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		fmt.Println("Input is piped", *invertPtr)
+		return
+	}
+
+	// file/dir mode
+	if len(args) < 2 {
+		log.Fatalln("Usage: gogrep [-r] [-v] <pattern> <filename|directory>")
+	}
+
+	target := args[1]
+
+	if *recursivePtr {
+		recursiveErr := filepath.WalkDir(target, func(path string, d os.DirEntry, err error) error {
+
+			if err != nil {
+				return err
+			}
+
+			if d.IsDir() {
+				return nil
+			}
+
+			if strings.ToLower(filepath.Ext(path)) != ".txt" {
+				return nil
+			}
+
+			searchFile(path, re)
+
+			return nil
+
+		})
+
+		if recursiveErr != nil {
+			fmt.Printf("Error walking the path %q: %v\n", target, err)
+		}
+	} else {
+		searchFile(target, re)
+	}
+
+}
+
+func searchFile(path string, re *regexp.Regexp) {
+	file, fileErr := os.Open(path)
+
+	if fileErr != nil {
+		log.Fatalf("failed to open file %v", fileErr)
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if re.MatchString(line) {
+			fmt.Printf("%s: %s\n", path, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading file %s: %v", path, err)
+	}
+}
